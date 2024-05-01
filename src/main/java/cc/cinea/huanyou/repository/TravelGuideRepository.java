9374c933e00f968af6b1a5ec65f0ffe51ba80085
张尧 <zhangyao@ft.tech>
2024-05-02 00:55:19 +0800

    feat: Update travelGuideRepo

--- src/main/java/cc/cinea/huanyou/repository/TravelGuideRepository.java
+++ src/main/java/cc/cinea/huanyou/repository/TravelGuideRepository.java
@@ -20,4 +20,12 @@ import java.util.List;
 public interface TravelGuideRepository extends JpaRepository<TravelGuide, Long> {
     @Query("select author as author from TravelGuide where id =:id")
     TravelGuideAuthorProjection findAuthorById(@Param("id") Long id);
+
+    @Query("select case when count(u)>0 then true else false end from TravelGuide g join g.likedUser u where g.id = :guideId and u.id = :userId")
+    boolean isLikedByUser(@Param("guideId") Long guideId, @Param("userId") Long userId);
+    Page<TravelGuidePreviewProjection> findAllBy(Pageable pageable);
+
+    Page<TravelGuidePreviewProjection> findAllByResort(Resort resortId, Pageable pageable);
+
+    List<TravelGuidePreviewProjection> findAllByAuthorOrderByPublishTimeDesc(RegisteredUser author);
 }
