1b7415410e1ecd8e6708d828eb879cd783028e66
Yupeng Zhu <1329361431@qq.com>
2024-05-11 11:35:23 +0800

    实现用户评论功能

---
+++ src/main/java/cc/cinea/huanyou/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package cc.cinea.huanyou.repository;
+
+import cc.cinea.huanyou.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
